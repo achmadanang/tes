dd620034db202706477194ab9335c4b5e8be87a2
achmadanang <89206078+achmadanang@users.noreply.github.com>
2022-01-13 23:58:39 +0700

    Add files via upload

---
+++ bio data.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout<<"NAMA: ACHMAD ANANG FIRMANSYAH \n";
+	cout<<"NPM: 06.2021.1.07417"<<endl;
+	cout<<"Jurusan Teknik Informatika";
+	cout<<"Fakultas FTETI"<<endl;
+}
